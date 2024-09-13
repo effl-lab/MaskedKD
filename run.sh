d2f32b06e687cffe9350f3d28869fedda2644b72
jgryu <ryu5038161@gmail.com>
2024-09-13 07:23:24 +0000

    edit run

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
-CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py \
+python main.py \
 --model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
---batch-size 512 --data-path /data/ILSVRC2012/ --distillation-type soft \
+--batch-size 512 --data-path ../dataset/tiny-imagenet-200 --distillation-type soft \
 --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
 --output_dir ./
+
+
+#python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py \
+#--model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
+#--batch-size 512 --data-path ./dataset --distillation-type soft \
+#--distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+#--output_dir ./
+
