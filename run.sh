3fdc904281e7477197240b8c14aa6059f2cb72d5
jgryu <ryu5038161@gmail.com>
2024-09-13 17:09:40 +0900

    add no distrbute mode

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python main.py \
+python main.py --no_distributed \
 --model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
 --batch-size 512 --data-path ../dataset/tiny-imagenet-200 --distillation-type soft \
 --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
