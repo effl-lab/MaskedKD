2522ef554181cb1ebc354ec451d6d3ad04e400f4
jgryu <ryu5938161@gmail.com>
2024-09-19 18:40:48 +0900

    test

--- engine.py
+++ engine.py
@@ -35,7 +35,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
             samples, targets = mixup_fn(samples, targets)
                                 
         # with torch.cuda.amp.autocast():
-        import ipdb; ipdb.set_trace()
+        # import ipdb; ipdb.set_trace()
 
         # with torch.autocast(device_type="hpu"):
         outputs, attn = model(samples)            
