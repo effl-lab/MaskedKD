b0835066e422d66ad4df07ecff2690a9d454e447
jgryu <ryu5938161@gmail.com>
2024-09-19 20:59:35 +0900

    test

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py \
+python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
 --model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
---batch-size 128 --data-path ../dataset/tiny-imagenet-200 --distillation-type soft \
+--batch-size 256 --data-path ../dataset/imagenet --distillation-type soft \
 --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
 --output_dir ./
 
