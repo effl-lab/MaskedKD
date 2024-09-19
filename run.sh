a5aadc1dca5bedaa4c83eb850843e50788c8fbf3
jgryu <ryu5938161@gmail.com>
2024-09-19 20:06:26 +0900

    test

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py \
+--model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
+--batch-size 128 --data-path ../dataset/tiny-imagenet-200 --distillation-type soft \
+--distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+--output_dir ./
+
+
 python main.py --no_distributed \
 --model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
 --batch-size 512 --data-path ../dataset/tiny-imagenet-200 --distillation-type soft \
@@ -5,9 +12,9 @@ python main.py --no_distributed \
 --output_dir ./
 
 
-#python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py \
-#--model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
-#--batch-size 512 --data-path ./dataset --distillation-type soft \
-#--distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
-#--output_dir ./
+python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py \
+--model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
+--batch-size 128 --data-path ./dataset --distillation-type soft \
+--distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+--output_dir ./
 
