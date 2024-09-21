049b516ef76638a77cb47e3a533d13138723a117
jgryu <ryu5038161@gmail.com>
2024-09-21 11:00:51 +0900

    maskedkd gaudi test

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
 --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
 --output_dir ./results | tee log_mask50.txt
 
-python main.py --no_distributed \
---model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
---batch-size 1024 --data-path ../dataset/imagenet --distillation-type soft \
---distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
---output_dir ./results | tee log_mask50.txt
+# python main.py --no_distributed \
+# --model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
+# --batch-size 1024 --data-path ../dataset/imagenet --distillation-type soft \
+# --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+# --output_dir ./results | tee log_mask50.txt
 
 # python3 run_timm_example.py \
 #     --model_name_or_path "timm/fastvit_t8.apple_in1k" \
