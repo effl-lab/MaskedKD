d5e587a028f1938921cecb0c4ed76c24e56c5f58
jgryu <ryu5038161@gmail.com>
2024-09-23 02:32:57 +0900

    maskedkd gaudi test

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ def get_args_parser():
 
     parser.add_argument('--output_dir', default='',
                         help='path where to save, empty for no saving')
-    parser.add_argument('--device', default='hpu:4',
+    parser.add_argument('--device', default='hpu',
                         help='device to use for training / testing')
     parser.add_argument('--seed', default=0, type=int)
     parser.add_argument('--resume', default='', help='resume from checkpoint')
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-# python -m torch.distributed.launch --no_distill --nproc_per_node=4 --use_env main.py \
-# --model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
-# --batch-size 256 --data-path ../imagenet --distillation-type soft \
-# --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
-# --output_dir ./results | tee log_mask50.txt
+python -m torch.distributed.launch --no_distill --nproc_per_node=4 --use_env main.py \
+--model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
+--batch-size 256 --data-path ../imagenet --distillation-type soft \
+--distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+--output_dir ./results | tee log_mask50.txt
 
 # python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
 # --model vit_small_patch16_224  --teacher_model deit3_small --epochs 300 \
@@ -10,11 +10,11 @@
 # --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
 # --output_dir ./results | tee log_mask50.txt
 
-python main.py --no_distributed --no_distill \
---model google/vit-base-patch16-224-in21k  --teacher_model deit3_small --epochs 300 \
---batch-size 128 --data-path ../imagenet --distillation-type soft \
---distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
---output_dir ./results | tee log_mask50.txt
+# python main.py --no_distributed --no_distill \
+# --model google/vit-base-patch16-224-in21k  --teacher_model deit3_small --epochs 300 \
+# --batch-size 128 --data-path ../imagenet --distillation-type soft \
+# --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+# --output_dir ./results | tee log_mask50.txt
 
 # python main.py --no_distributed --no_distill --hf_model \
 # --model google/vit-base-patch16-224-in21k  --teacher_model deit3_small --epochs 300 \
