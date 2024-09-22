1c799be66095f7fc5b1b9c2627485e2f0b509c1a
jgryu <ryu5038161@gmail.com>
2024-09-23 02:53:06 +0900

    maskedkd gaudi test

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
---no_distill \
---model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
---batch-size 256 --data-path ../imagenet --distillation-type soft \
---distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
---output_dir ./results | tee log_mask50.txt
+# python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
+# --no_distill \
+# --model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
+# --batch-size 256 --data-path ../imagenet --distillation-type soft \
+# --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+# --output_dir ./results | tee log_mask50.txt
 
 # python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
 # --model vit_small_patch16_224  --teacher_model deit3_small --epochs 300 \
@@ -11,11 +11,12 @@ python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
 # --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
 # --output_dir ./results | tee log_mask50.txt
 
-# python main.py --no_distributed --no_distill \
-# --model google/vit-base-patch16-224-in21k  --teacher_model deit3_small --epochs 300 \
-# --batch-size 128 --data-path ../imagenet --distillation-type soft \
-# --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
-# --output_dir ./results | tee log_mask50.txt
+python main.py --no_distributed \
+--no_distill \
+--model google/vit-base-patch16-224-in21k  --teacher_model deit3_small --epochs 300 \
+--batch-size 256 --data-path ../imagenet --distillation-type soft \
+--distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
+--output_dir ./results | tee log_mask50.txt
 
 # python main.py --no_distributed --no_distill --hf_model \
 # --model google/vit-base-patch16-224-in21k  --teacher_model deit3_small --epochs 300 \
