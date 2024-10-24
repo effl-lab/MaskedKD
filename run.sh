0080bdefac2e278c05b45c89d299351247915a35
손승우 <97292370+seungwoo-s@users.noreply.github.com>
2024-10-24 20:58:40 +0900

    Update

--- run.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py \
---model deit_tiny_patch16_224  --teacher_model deit3_small --epochs 300 \
---batch-size 512 --data-path /data/ILSVRC2012/ --distillation-type soft \
---distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --len_num_keep 98 \
---output_dir ./
