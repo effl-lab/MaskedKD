a5d2af67e2e72f1fe0e04c2894547114d3c540c4
jgryu <ryu5038161@gmail.com>
2024-12-18 17:24:46 +0900

    add runsh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,5 +1,12 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
---model deit_small_patch16_224  --teacher_model deit_base --epochs 300 \
---batch-size 256 --data-path /data/ILSVRC2012 --distillation-type soft \
---distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --maskedkd --len_num_keep 98 \
---output_dir ./output_test | tee ./test_log.txt
+# CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
+# --model deit_small_patch16_224  --teacher_model deit_base --epochs 300 \
+# --batch-size 256 --data-path /data/ILSVRC2012 --distillation-type soft \
+# --distillation-alpha 0.5 --distillation-tau 1  --input-size 224 --maskedkd --len_num_keep 98 \
+# --output_dir ./output_test | tee ./test_log.txt
+
+mpirun -n 4 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+python -u main.py \
+--model deit_tiny_patch16_224 --teacher_model deit3_small --epochs 2 \
+--batch-size 256 --data-path ../imagenet --distillation-type soft \
+--distillation-alpha 0.5 --distillation-tau 1 --input-size 224 --maskedkd --len_num_keep 98 \
+--output_dir ./results | tee -a log_.txt
